d36c0766bf6813bfa0f22ff8706bbd98ebdb157b
Sanjana Maurya <sanjana@foundercodes.com>
2024-07-19 13:14:36 +0530

    BigMoney

---
+++ android/app/src/main/kotlin/com/treamz/plinko/flutter_plinko/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.foundercode.bigmoney
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/res/app_constant.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appName = 'bigmoney';
+  static const String appVersion = '1.0.3';
+}
+
